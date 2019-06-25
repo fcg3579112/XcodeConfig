2cec0d3cf3b1ccf0470cb67e511413dfccca0478
hm <edz@hyj-MacBook-Pro.local>
2019-06-25 19:35:31 +0800

    add libstdc++.6.0.9.dylib script

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 ### 问题
 
-升级 **Xcode10+** 后，老项目或者第三方依旧依赖 **libstdc++.6.0.9.tbd** 文件，缺少文件工程报错，本脚本将 ***libstdc++.6.0.9.tbd*** 文件添加到 Xcode 对应的目录内(含: iPhoneOS、iPhoneSimulator、MacOSX)即可。 
+Xcode10 废弃了 **libstdc++** 库
+
+- libstdc++.6.0.9.tbd
+- libstdc++.6.tbd  (libstdc++.6.0.9.tbd的替身)
+- libstdc++.tbd    (libstdc++.6.0.9.tbd的替身)
+- libstdc++.6.0.9.dylib
+- libstdc++.6.dylib (libstdc++.6.0.9.dylib的替身)
+- libstdc++.dylib (libstdc++.6.0.9.dylib的替身)
+
+删除以上库后编译 老项目或者第三方库会出现 Error 和 Crash。本脚本将 ***libstdc++.6.0.9.tbd*** 和 ***libstdc++.6.0.9.dylib*** 文件恢复到 Xcode 对应的目录内(含: iPhoneOS、iPhoneSimulator、MacOSX、CoreSimulator)。 
 
 ### 使用
 
@@ -18,7 +27,7 @@ sh add_stdc++_to_Xcode.sh
 sudo sh add_stdc++_to_Xcode.sh
 ```
 
-![](http://ww3.sinaimg.cn/large/006tNc79ly1g4dfejicvtj30ne0g6goy.jpg)
+![](http://ww3.sinaimg.cn/large/006tNc79ly1g4dmb7ohtbj30ex0ldgov.jpg)
 
 ### 安全
 
@@ -27,25 +36,26 @@ sudo sh add_stdc++_to_Xcode.sh
 |iPhoneOS|6d6e5745a55f1485055d74a574e31fc2|
 |iPhoneSimulator|a2969039a4c5066e9eb4dbe0405ecae9|
 |MacOSX|3d646f322a9ef63533429a0ae1ca249d|
+|CoreSimulator|7cf875f4b851dcdeece9f8825321b423|
 
 添加前请核查MD5值, 文件被修改的，与本分享无关。
 
 ### 问题
-- 1、如果原来Xcode有该文件会不会被替换？
+- 1、如果Xcode已有该文件会不会被替换？
 
  答：不会覆盖已有文件，执行会有如下提示.
 
-![](http://ww4.sinaimg.cn/large/006tNc79ly1g4dfhbsg2aj30qs0aamz2.jpg)
+![](http://ww3.sinaimg.cn/large/006tNc79ly1g4dla334olj31gi0juq7v.jpg)
 
 - 2、如果Xcode安装目录非默认目录/Applications，怎么安装成功？
 
  答：执行脚本会给出如下提示：
 
-![](http://ww2.sinaimg.cn/large/006tNc79ly1g4dfku0mvvj30to0aiac4.jpg)
+![](http://ww1.sinaimg.cn/large/006tNc79ly1g4dlbxvungj30yw0f4acu.jpg)
 
 默认Xcode 是安装在 /Applications 目录下，并且Xcode的名称为: `Xcode.app`, 如果有多个Xcode，请自行修改脚本内的 Xcode 安装目录。如图所示: 
 
-![](http://ww4.sinaimg.cn/large/006tNc79ly1g4dfb1y26sj31f20di0we.jpg)
+![](http://ww1.sinaimg.cn/large/006tNc79ly1g4dle03fmvj30wg0ayact.jpg)
 
 - 3、遇到权限问题，如**cp: /Applications/Xcode.app/.../usr/lib/libstdc++.6.0.9.tbd: Permission denied**, 怎么办？
 
@@ -56,8 +66,14 @@ sudo sh add_stdc++_to_Xcode.sh
 	sudo sh add_stdc++_to_Xcode.sh
 ```
 
- 
  ![](http://ww3.sinaimg.cn/large/006tNc79ly1g4dgdge10qj315l0u07bo.jpg)
+ 
+ - 4、编译报错 **Reason: no suitable image found.  Did find:
+	/usr/lib/libstdc++.6.dylib: mach-o, but not built for iOS simulator** 是什么问题？
+
+答：Xcode10+ 版本缺少libstdc++.6.0.9.dylib文件造成无法加载dylib，执行本脚本就会自动将文件复原到原目录内。报错如下：
+
+![](http://ww1.sinaimg.cn/large/006tNc79ly1g4dmnd1yjfj31ek072ac7.jpg)
 
 
 
--- stdc++.6.0.9/add_stdc++_to_Xcode.sh
+++ stdc++.6.0.9/add_stdc++_to_Xcode.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
+__iPhoneOS_To_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib"
+__iPhoneSimulator_To_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib"
+__MacOSX_To_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib"
+__CoreSimulator_To_Path="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib"
+
 __STDC_PLUS_PLUS_NAME="libstdc++.6.0.9.tbd"
 __STDC_PLUS_PLUS_LN1_NAME="libstdc++.6.tbd"
 __STDC_PLUS_PLUS_LN2_NAME="libstdc++.tbd"
 
-__iPhoneOS_To_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib"
-__iPhoneSimulator_To_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib"
-__MacOSX_To_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib"
+__STDC_PLUS_PLUS_Dylib_NAME="libstdc++.6.0.9.dylib"
+__STDC_PLUS_PLUS_Dylib_LN1_NAME="libstdc++.6.dylib"
+__STDC_PLUS_PLUS_Dylib_LN2_NAME="libstdc++.dylib"
 
 # 以下勿动
 __SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 __iPhoneOS_PATH="${__SHELL_FOLDER}/Resources/iPhoneOS/${__STDC_PLUS_PLUS_NAME}"
 __iPhoneSimulator_PATH="${__SHELL_FOLDER}/Resources/iPhoneSimulator/${__STDC_PLUS_PLUS_NAME}"
 __MacOSX_PATH="${__SHELL_FOLDER}/Resources/MacOSX/${__STDC_PLUS_PLUS_NAME}"
+__CoreSimulator_PATH="${__SHELL_FOLDER}/Resources/CoreSimulator/${__STDC_PLUS_PLUS_Dylib_NAME}"
 
 function print_ok_message() {
   echo "\033[32m${1}\033[0m"
@@ -28,9 +34,12 @@ function cp_ln_file() {
   cur_name=$1
   form_path=$2
   to_path=$3
-  std_orig_path="${to_path}/${__STDC_PLUS_PLUS_NAME}"
-  std_ln1_path="${to_path}/${__STDC_PLUS_PLUS_LN1_NAME}"
-  std_ln2_path="${to_path}/${__STDC_PLUS_PLUS_LN2_NAME}"
+  orig_name=$4
+  ln1_name=$5
+  ln2_name=$6
+  std_orig_path="${to_path}/${orig_name}"
+  std_ln1_path="${to_path}/${ln1_name}"
+  std_ln2_path="${to_path}/${ln2_name}"
 
   if [[ -d "${to_path}" ]]; then
     cp $form_path $to_path
@@ -39,21 +48,21 @@ function cp_ln_file() {
 
       ln -s $std_orig_path $std_ln1_path
       ln -s $std_orig_path $std_ln2_path
-      print_ok_message "${cur_name} 文件 ${__STDC_PLUS_PLUS_NAME} 添加成功...\n添加路径=${to_path}\n"
+      print_ok_message "${cur_name} 文件 ${orig_name} 添加成功...\n添加路径=${to_path}\n"
     else
-      print_fail_message "${cur_name} 文件 ${__STDC_PLUS_PLUS_NAME} 添加失败..."
+      print_fail_message "${cur_name} 文件 ${orig_name} 添加失败..."
     fi
 
     if [[ -f "${std_ln1_path}" ]]; then
-      print_ok_message "${cur_name} 文件 ${__STDC_PLUS_PLUS_LN1_NAME} 添加成功...\n添加路径=${to_path}\n"
+      print_ok_message "${cur_name} 文件 ${ln1_name} 添加成功...\n添加路径=${to_path}\n"
     else
-      print_fail_message "${cur_name} 文件 ${__STDC_PLUS_PLUS_LN1_NAME} 添加失败..."
+      print_fail_message "${cur_name} 文件 ${ln1_name} 添加失败..."
     fi
 
     if [[ -f "${std_ln2_path}" ]]; then
-      print_ok_message "${cur_name} 文件 ${__STDC_PLUS_PLUS_LN2_NAME} 添加成功...\n添加路径=${to_path}\n"
+      print_ok_message "${cur_name} 文件 ${ln2_name} 添加成功...\n添加路径=${to_path}\n"
     else
-      print_fail_message "${cur_name} 文件 ${__STDC_PLUS_PLUS_LN2_NAME} 添加失败...\n"
+      print_fail_message "${cur_name} 文件 ${ln2_name} 添加失败...\n"
     fi
   else
     print_fail_message "${cur_name} 添加失败, 请核查 Xcode 是否安装在 /Applications/Xcode.app\n"
@@ -64,19 +73,32 @@ function cp_ln_file() {
 if [[ -f "${__iPhoneOS_To_PATH}/${__STDC_PLUS_PLUS_NAME}" ]]; then
   print_fail_message "iPhoneOS 文件 ${__STDC_PLUS_PLUS_NAME} 已经存在，无需重复添加...\n添加路径=${__iPhoneOS_To_PATH}\n"
 else
-  cp_ln_file "iPhoneOS" $__iPhoneOS_PATH $__iPhoneOS_To_PATH
+  cp_ln_file "iPhoneOS" $__iPhoneOS_PATH $__iPhoneOS_To_PATH $__STDC_PLUS_PLUS_NAME $__STDC_PLUS_PLUS_LN1_NAME $__STDC_PLUS_PLUS_LN2_NAME
 fi
+
+print_ok_message "======================="
 
 # iPhoneSimulator
 if [[ -f "${__iPhoneSimulator_To_PATH}/${__STDC_PLUS_PLUS_NAME}" ]]; then
   print_fail_message "iPhoneSimulator 文件 ${__STDC_PLUS_PLUS_NAME} 已经存在，无需重复添加...\n添加路径=${__iPhoneSimulator_To_PATH}\n"
 else
-  cp_ln_file "iPhoneSimulator" $__iPhoneSimulator_PATH $__iPhoneSimulator_To_PATH
+  cp_ln_file "iPhoneSimulator" $__iPhoneSimulator_PATH $__iPhoneSimulator_To_PATH $__STDC_PLUS_PLUS_NAME $__STDC_PLUS_PLUS_LN1_NAME $__STDC_PLUS_PLUS_LN2_NAME
 fi
+
+print_ok_message "======================="
 
 # MacOSX
 if [[ -f "${__MacOSX_To_PATH}/${__STDC_PLUS_PLUS_NAME}" ]]; then
   print_fail_message "MacOSX 文件 ${__STDC_PLUS_PLUS_NAME} 已经存在，无需重复添加...\n添加路径=${__MacOSX_To_PATH}\n"
 else
-  cp_ln_file "MacOSX" $__MacOSX_PATH $__MacOSX_To_PATH
+  cp_ln_file "MacOSX" $__MacOSX_PATH $__MacOSX_To_PATH $__STDC_PLUS_PLUS_NAME $__STDC_PLUS_PLUS_LN1_NAME $__STDC_PLUS_PLUS_LN2_NAME
+fi
+
+print_ok_message "======================="
+
+# CoreSimulator
+if [[ -f "${__CoreSimulator_To_Path}/${__STDC_PLUS_PLUS_Dylib_NAME}" ]]; then
+  print_fail_message "CoreSimulator 文件 ${__STDC_PLUS_PLUS_Dylib_NAME} 已经存在，无需重复添加...\n添加路径=${__CoreSimulator_To_Path}\n"
+else
+  cp_ln_file "CoreSimulator" $__CoreSimulator_PATH $__CoreSimulator_To_Path $__STDC_PLUS_PLUS_Dylib_NAME $__STDC_PLUS_PLUS_Dylib_LN1_NAME $__STDC_PLUS_PLUS_Dylib_LN2_NAME
 fi
